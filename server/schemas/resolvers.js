b32b67e6bd72327226f6282ae018bee70610f44d
Josiah-Canada <josiahcanada@yahoo.com>
2022-08-17 17:32:12 -0400

    working on it

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,50 @@
+const { AuthenticationError } = require("apollo-server-express");
+const {User} = require('../models');
+const {signToken} = require('../utils/auth')
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+      me: async (parent, arg, context) => {
+        if(context.user){
+            const foundUser = await User.findOne({_id: context.user._id}).select('-__v -password');
+            return foundUser;
+        }
+        
       }
+    },
+    Mutation: {
+        addUser:async (parent, {email, password, username}) => {
+            return await User.create({email, password, username});
+            
+
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+
+            if(!user){
+                throw new AuthenticationError('Wrong Info!')
+            }
+
+            const correctPassword = user.isCorrectPasswrod(password);
+
+            if(!correctPassword){
+                throw new AuthenticationError('Wrong Info!')
+            }
+            
+            const token = signToken(user);
+            return {token, user}
+        },
+        saveBook: { 
+           
+
+
+
+
+        },
+        removeBook: {
+            
+
+        }
     }
   };
   
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,43 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  email: String
+  password: [Password]
+}
+
+type Book {
+  _id: ID
+  Description: String
+  createdAt: String
+  username: String
+  email: Int
+  title: String
+  image: Image
+  link:Link
+  reactions: [Reaction]
+}
+
+type Description {
+  _id: ID
+  descriptionBody: String
+  createdAt: String
+  username: String
+}
+
 type Query {
-    helloWorld: String
-  }
+  users: [User]
+  user(username: String!): User
+  password(password: String): [Password]
+  books(_id: ID!): Book
+}
+
+type Mutation {
+  login(email: String!, password: String!): User
+  addUser(username: String!, email: String!, password: String!): User
+}
   `;
 
 // export the typeDefs
